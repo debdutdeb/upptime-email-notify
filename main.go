c45cf01aa07a19cefff45647ea319b2f4b3f2ba4
Debdut Chakraborty <imdebdut@protonmail.com>
2021-02-24 18:07:04 +0530

    Using two files for such a small program seems unnecessary.

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	log "github.com/sirupsen/logrus"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/smtp"
 	"os"
 	"regexp"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -91,6 +94,63 @@ func handler(rw http.ResponseWriter, rq *http.Request) {
 		}
 	}()
 
+}
+
+func getEnv(vari string) (val string) {
+	var e bool
+	if val, e = os.LookupEnv(vari); !e {
+		log.Fatal("Environment variable " + vari + " is not set.")
+	}
+	return
+}
+
+func sendMails(subject, body string) error {
+
+	var (
+		user,
+		password,
+		host,
+		port,
+		from string
+		to []string
+	)
+
+	if _, e := os.LookupEnv("NOTIFICATION_EMAIL_SENDGRID"); e {
+		user = "apikey"
+		password = getEnv("NOTIFICATION_EMAIL_SENDGRID_API_KEY")
+		host = "smtp.sendgrid.net"
+		port = "587"
+	} else {
+		user = getEnv("NOTIFICATION_EMAIL_SMTP_USERNAME")
+		password = getEnv("NOTIFICATION_EMAIL_SMTP_PASSWORD")
+		host = getEnv("NOTIFICATION_EMAIL_SMTP_HOST")
+		port = getEnv("NOTIFICATION_EMAIL_SMTP_PORT")
+	}
+
+	from = getEnv("NOTIFICATION_EMAIL_FROM")
+	to = strings.Fields(getEnv("NOTIFICATION_EMAIL_TO"))
+
+	auth := smtp.PlainAuth("", user, password, host)
+	msg := []byte("From: " + from + "\r\n" +
+		"To: " + to[0] + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n" +
+		"\r\n" +
+		body + "\r\n")
+
+	log.Debug("NOTIFICATION_EMAIL_SMTP_USERNAME: " + user + "\n" +
+		"NOTIFICATION_EMAIL_SMTP_PASSWORD: " + password + "\n" +
+		"NOTIFICATION_EMAIL_SMTP_HOST: " + host + "\n" +
+		"NOTIFICATION_EMAIL_SMTP_PORT: " + port + "\n" +
+		"NOTIFICATION_EMAIL_FROM: " + from + "\n" +
+		"NOTIFICATION_EMAIL_TO: " + strings.Join(to, " "))
+
+	if err := smtp.SendMail(
+		host+":"+port, auth, from, to, msg); err != nil {
+		return fmt.Errorf("sendMails failed. %v", err)
+	}
+
+	return nil
 }
 
 func main() {
